5d24fa618e1fc7c098614bd870d342f5bb6255ad
JeongTaekCho <92679073+JeongTaekCho@users.noreply.github.com>
2022-08-05 00:30:22 +0900

    modify cyworld day4

--- cyworld/js/game.js
+++ cyworld/js/game.js
@@ -20,3 +20,19 @@ function getLottoNum() {
     }
   }
 }
+
+const startWord = () => {
+  let myWord = document.getElementById("myWord").value;
+  let word = document.getElementById("word").innerText;
+  let firstWord = myWord[0];
+  let lastWord = word[word.length - 1];
+
+  if (firstWord === lastWord) {
+    document.getElementById("result").innerText = "정답입니다.";
+    document.getElementById("word").innerText = myWord;
+    document.getElementById("myWord").value = "";
+  } else {
+    document.getElementById("result").innerText = "땡!!";
+    document.getElementById("myWord").value = "";
+  }
+};
